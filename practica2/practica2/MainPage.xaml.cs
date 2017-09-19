de5eaaaf7d3a399661228f47b5dc4351c4bb2819
mario alberto vazquez <scorpion.wb82>
2017-09-18 22:25:01 -0500

    Agregar archivos de proyecto.

---
+++ practica2/practica2/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace practica2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void click( object sender, EventArgs e)
+        {
+            var nombre = txtNombre.Text;
+            var apellidos = txtApellidos.Text;
+            var direccion = txtDireccion.Text;
+            var telefono = txttelefono.Text;
+            var carrera = txtcarrera.Text;
+            var semestre = txtsemestre.Text;
+            var matricula = txtmatricula.Text;
+            var correo = txtcorreo.Text;
+            var github = txtgithub.Text;
+            DisplayAlert("datos Ingresados ", nombre +"\n"+apellidos+"\n"+direccion+"\n"+telefono+"\n" +carrera+"\n" +semestre+"\n" +matricula+"\n" +correo+"\n" +github+"\n","Listo");
+        }
+    }
+}
